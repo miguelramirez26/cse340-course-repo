d3a1b7f1d2c1bbd0b3eddd89270b35073f2d14db
miguelramirez26 <26ramirez.m@gmail.com>
2026-06-04 14:19:50 +0200

    Implement complete authentication system and secure logout flow

--- src/models/users.js
+++ src/models/users.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt'
 import db from './db.js'
 
 const createUser = async (name, email, passwordHash) => {
@@ -22,4 +23,41 @@ const createUser = async (name, email, passwordHash) => {
     return result.rows[0].user_id;
 };
 
-export { createUser };
+const findUserByEmail = async (email) => {
+    const query = `
+        SELECT user_id, name, email, password_hash, role_id 
+        FROM users 
+        WHERE email = $1
+    `;
+    const queryParams = [email];
+    
+    const result = await db.query(query, queryParams);
+
+    if (result.rows.length === 0) {
+        return null; // User not found
+    }
+    
+    return result.rows[0];
+};
+
+const verifyPassword = async (password, passwordHash) => {
+    return bcrypt.compare(password, passwordHash);
+};
+
+const authenticateUser = async (email, password) => {
+    const user = await findUserByEmail(email);
+
+    if (user === null) {
+        return null;
+    }
+
+    const isMatch = await verifyPassword(password, user.password_hash);
+
+    if (isMatch === false) {
+        return null;
+    }
+
+    return user;
+}
+
+export { createUser, authenticateUser };
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganization
 import { showProjectsPage, showProjectDetailsPage, showNewProjectForm, processNewProjectForm, projectValidation, showEditProjectForm, processEditProjectForm } from './controllers/projects.js';
 import { showCategoriesPage, getCategoryDetails, showAssignCategoriesForm, processAssignCategoriesForm, showCreateCategoryForm, processCreateCategory, showEditCategoryForm, processEditCategory, categoryValidation } from './controllers/categories.js';
 import { triggerTestError } from './controllers/errors.js';
-import { showUserRegistrationForm, processUserRegistrationForm } from './controllers/users.js';
+import { showUserRegistrationForm, processUserRegistrationForm, showLoginForm, processLoginForm, processLogout } from './controllers/users.js';
 
 const router = express.Router();
 
@@ -57,5 +57,10 @@ router.post('/edit-category/:id', categoryValidation, processEditCategory);
 // User registration routes
 router.get('/register', showUserRegistrationForm);
 router.post('/register', processUserRegistrationForm);
+
+// User login and logout router
+router.get('/login', showLoginForm);
+router.post('/login', processLoginForm);
+router.get('/logout', processLogout);
 
 export default router;
